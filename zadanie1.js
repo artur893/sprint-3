bc5d2bd95945c109609f39c70979ff3fc07229a5
Artur Halemba <106431606+artur893@users.noreply.github.com>
2025-10-05 11:23:22 +0200

    Add files via upload

---
+++ zadanie1.js
@@ -0,0 +1,24 @@
+const userName = "sara";
+const userAge = 25;
+const likesJavaScript = false;
+const userHobby = "painting";
+const userCity = "Cracow";
+const capitalUsername = userName.charAt(0).toUpperCase() + userName.slice(1);
+const futureAge = userAge + 10;
+const isAdult = userAge >= 18;
+const isWarsawCitizen = userCity.toLowerCase() === "warsaw";
+const likesJSAndIsAdult = likesJavaScript && isAdult;
+
+let greeting = `Hello ${capitalUsername}\nAge: ${userAge}`;
+
+if (likesJavaScript) {
+  greeting += "\nLikes JavaScript";
+}
+
+if (isWarsawCitizen) {
+  greeting += "\nWarsaw citizen";
+}
+
+greeting += `\nHobby is ${userHobby}`;
+
+console.log(greeting);
